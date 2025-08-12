e2a917fdf0d34a5d620a4ba7dd07618f95c16a3e
gbrreel <gabrielggalloo@gmail.com>
2025-08-12 20:23:59 -0300

    Inicialização do projeto

---
+++ src/main/java/com/gabriel/tradutor/tradutor_ia/TradutorIaApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.tradutor.tradutor_ia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradutorIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradutorIaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabriel/tradutor/tradutor_ia/TradutorIaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.tradutor.tradutor_ia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradutorIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
